d21f2a5885049cc0e554076daee898ba0c11e73c
David Calle <davidcalleg@gmail.com>
2022-03-17 14:10:20 -0500

    initial implementation for multiple card types

--- tuya-credit-fe/components/results/amortizationTable/index.tsx
+++ tuya-credit-fe/components/results/amortizationTable/index.tsx
@@ -1,19 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import FeesTable from './FeesTable'
-import { getDecreasingFees } from '../../../utils/helpers'
+import { getDecreasingFees, getAnnuityFees } from '../../../utils/helpers'
+import { fee } from '../../../types'
 
 type Props = {
     totalAmount: number,
     feesNumber: number,
     handlingFee: number,
-    interestRate: number
+    interestRate: number,
+    cardType: string
 }
 
-const AmortizationTable = ({ totalAmount, feesNumber, handlingFee, interestRate }: Props) => {
-
-    const fees = getDecreasingFees(feesNumber, interestRate, totalAmount, handlingFee)
-
+const AmortizationTable = ({ totalAmount, feesNumber, handlingFee, interestRate, cardType }: Props) => {
+    let fees: Array<fee> = [];
+    if (cardType === "mastercard") {
+        fees = getDecreasingFees(feesNumber, interestRate, totalAmount, handlingFee)
+    }
+    else {
+        fees = getAnnuityFees(feesNumber, interestRate, totalAmount, handlingFee)
+    }
     return (
         <FeesTable fees={fees} />
     )
--- tuya-credit-fe/utils/helpers.ts
+++ tuya-credit-fe/utils/helpers.ts
@@ -1,12 +1,38 @@
 
 import { fee } from '../types'
 
-export const getAnnuityFees = () => {
-
-}
-
 export const currencyFormat = (num: Number) => {
     return '$' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+export const getAnnuityFees = (feesNumber:number, interestRate:number, totalAmount:number, handlingFee: number) => {
+    let annuity = totalAmount * ( Math.pow((1 + interestRate), feesNumber) * interestRate / (Math.pow((1 + interestRate), feesNumber) - 1));
+    const buyMonth: Date = new Date();
+    const buyFee: fee = {
+        date: buyMonth.toLocaleDateString('es_ES'),
+        value: currencyFormat(0),
+        amortizationValue: currencyFormat(0),
+        balance: currencyFormat(totalAmount),
+        number : 0,
+        handlingFeeValue: currencyFormat(0)
+    }
+
+    const fees: Array<fee> = [buyFee]
+
+    for (let i = 2; i <= feesNumber; i++) {
+        buyMonth.setMonth(buyMonth.getMonth() + 1);
+        fees.push({
+            date: buyMonth.toLocaleDateString('es_ES'),
+            value: currencyFormat(annuity),
+            amortizationValue: currencyFormat(annuity),
+            interestValue: currencyFormat(0),
+            balance: currencyFormat( annuity * (  (Math.pow((1 + interestRate), feesNumber-i+1) - 1) / (Math.pow((1 + interestRate), feesNumber-i+1) * interestRate))),
+            number: i,
+            handlingFeeValue: currencyFormat(handlingFee)
+        });
+    }
+
+    return fees;
 }
 
 export const getDecreasingFees = (feesNumber:number, interestRate:number, totalAmount:number, handlingFee: number) =>{
@@ -16,11 +42,11 @@ export const getDecreasingFees = (feesNumber:number, interestRate:number, totalA
 
     const buyFee: fee = {
         date: buyMonth.toLocaleDateString('es_ES'),
-        value: '0',
-        amortizationValue: '0',
+        value: currencyFormat(0),
+        amortizationValue: currencyFormat(0),
         balance: currencyFormat(totalAmount),
         number : 0,
-        handlingFeeValue: '0'
+        handlingFeeValue: currencyFormat(0)
     }
 
     buyMonth.setMonth(buyMonth.getMonth() + 1);
