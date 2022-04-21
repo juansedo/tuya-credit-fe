1d3f3ac3288b25f81645c95b133a7da9b99bdf22
David Calle <davidcalleg@gmail.com>
2022-04-21 08:28:01 -0500

    enhance login styles and add tuya logo

--- screens/Login/LoginScreen.tsx
+++ screens/Login/LoginScreen.tsx
@@ -1,9 +1,7 @@
-import { StyleSheet, FlatList, Button, Text, TextInput } from 'react-native';
+import { StyleSheet, Image, Button, Text, TextInput } from 'react-native';
 import { View } from '../../components/Themed';
-import ProductSimulationCell from '../../components/simulation/ProductSimulationCell';
 import React, { useState, useEffect, useContext } from 'react'
 import { CartContext } from '../../utils/cart-context';
-import { ProductDTO } from '../../types';
 import { AppColors } from '../../constants/Colors'
 
 
@@ -13,26 +11,19 @@ interface LoginScreenProps {
 
 export default function LoginScreen(props: LoginScreenProps) {
 
-  const { state, dispatch } = useContext(CartContext);
-
-  const finance = () => {
-    dispatch({ type: "SIMULATE" });
-    props.navigation.navigate('ResultTab');
-  }
-
   return (
     <View style={styles.container}>
       <View style={styles.login}>
         <View style={styles.title}>
-          <View style={styles.logo}></View>
+          <Image style={styles.logo} source={require('../../assets/images/credituya-white.png')} />
           <Text style={[styles.text, styles.titleText]}>Inicio de sesión</Text>
         </View>
         <View style={styles.inputs}>
-          <TextInput style={styles.input}></TextInput>
-          <TextInput style={styles.input}></TextInput>
+          <TextInput style={styles.input} placeholder="correo"></TextInput>
+          <TextInput style={styles.input} placeholder="contraseña"></TextInput>
         </View>
         <View style={styles.submit}>
-          <Text style={[styles.text, styles.forgotPassword]}>olvidaste tu contraseña</Text>
+          <Text style={[styles.text, styles.forgotPassword]}>¿olvidaste tu contraseña?</Text>
           <Button title={'Aceptar'} color={AppColors.redColor} onPress={() => console.log('')} />
         </View>
       </View>
@@ -55,11 +46,10 @@ const styles = StyleSheet.create({
 
   },
   logo: {
-    width: 200,
-    height: 100,
-    backgroundColor: '#fff',
+    width: '80%',
     marginHorizontal: 'auto',
     alignSelf: 'center',
+    resizeMode: 'contain'
   },
   text: {
     color: '#fff',
@@ -72,6 +62,8 @@ const styles = StyleSheet.create({
   titleText: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginTop: -10,
+    lineHeight: 20,
   },
   inputs: {
     marginBottom: '30%',
@@ -80,15 +72,19 @@ const styles = StyleSheet.create({
   input: {
     marginBottom: 10,
     backgroundColor: '#fff',
-    width: '80%',
+    width: '100%',
     alignSelf: 'center',
     height: 40,
+    paddingHorizontal: 10,
   },
   submit: {
     backgroundColor: AppColors.redColor,
+    width: '90%',
+    alignSelf: 'center',
   },
   forgotPassword: {
-    marginBottom: 10
+    marginBottom: 10,
+    fontWeight: 'bold',
   }
 
 });
