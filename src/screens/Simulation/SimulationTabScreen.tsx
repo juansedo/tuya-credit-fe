c62ac5ad64ca4946fd05507e8c7dba5a201885ea
Juan Sebastián Díaz Osorio <jsdiazo@eafit.edu.co>
2022-05-28 16:37:14 -0500

    refactoring

--- src/screens/Simulation/SimulationTabScreen.tsx
+++ src/screens/Simulation/SimulationTabScreen.tsx
@@ -10,39 +10,6 @@ import { ProductItem } from "_types"
 import { useAsync } from "_utils/hooks/useAsync";
 import { AuthContext } from "_utils/auth-context";
 
-const creditCards = [
-  {
-    type: "PRIVADA",
-    lastDigits: "2291",
-    fee: "21500.00",
-    feeMode: "MONTHLY IN USE",
-    capacity: "8600000.00",
-    available: "5160000.00",
-    interestRate: "0.1600",
-    userKey: "QRST"
-  },
-  {
-    type: "CREDICOMPRAS",
-    lastDigits: "7263",
-    fee: "12500.00",
-    feeMode: "MONTHLY",
-    capacity: "16200000.00",
-    available: "14580000.00",
-    interestRate: "0.9300",
-    userKey: "QRST"
-  },
-  {
-    type: "PRIVADA",
-    lastDigits: "2993",
-    fee: "21500.00",
-    feeMode: "MONTHLY",
-    capacity: "17600000.00",
-    available: "10560000.00",
-    interestRate: "1.9700",
-    userKey: "QRST"
-  },
-]
-
 interface SimulationTabScreenProps {
   navigation: any;
 }
@@ -109,7 +76,6 @@ const SimulationTabScreen = (props: SimulationTabScreenProps) => {
         }}
         onClose={() => setFinanceModalVisible(false)}
       />
-      {/* <ModalFinance navigation={props.navigation} modalVisible={modalFinanceVisible} setModalVisible={setFinanceModalVisible} cards={creditCards} /> */}
     </View>
   );
 };
